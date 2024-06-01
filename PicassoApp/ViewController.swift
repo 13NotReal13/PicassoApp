bedfb716f0cfd5770abe2d40d929a1c629c9becf
Ivan Semikin <i.semikin.94@icloud.com>
2024-06-01 12:25:30 +0200

    Initial Commit

---
+++ PicassoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PicassoApp
+//
+//  Created by Иван Семикин on 01/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
